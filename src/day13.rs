d396c73f885c60f76769f091af46840dd9eba4cc
tmokenc <tmokenc@protonmail.com>
2022-07-11 01:43:54 +0900

    day13

---
+++ src/day13.rs
@@ -0,0 +1,58 @@
+pub struct ShuttleSearch {
+    timestamp: u64,
+    buses: Vec<Option<u64>>,
+}
+
+impl crate::AdventOfCode for ShuttleSearch {
+    const TITLE: &'static str = "Shuttle Search";
+    const DAY: u8 = 13;
+
+    fn new(input: &str) -> Option<Self> {
+        let mut iter = input.lines();
+        let timestamp = iter.next()?.parse::<u64>().ok()?;
+        let buses = iter
+            .next()?
+            .split(',')
+            .map(|v| v.parse::<u64>().ok())
+            .collect();
+
+        Some(Self { timestamp, buses })
+    }
+
+    fn part1(&self) -> u64 {
+        self.buses
+            .iter()
+            .filter_map(|v| {
+                let val = v.as_ref().copied()?;
+                let loops = self.timestamp as f64 / val as f64;
+                let loops = loops.ceil() as u64;
+                let next = val * loops - self.timestamp;
+                Some((val, next))
+            })
+            .min_by_key(|v| v.1)
+            .map(|(v, next)| v * next)
+            .unwrap_or(0)
+    }
+
+    fn part2(&self) -> u64 {
+        let data: Vec<(u64, u64)> = self
+            .buses
+            .iter()
+            .zip(0..)
+            .filter_map(|(&v, i)| v.zip(Some(i)))
+            .collect();
+
+        let mut timestamp = 0;
+        let mut inc = 1;
+
+        for (bus, offset) in data {
+            while (timestamp + offset) % bus != 0 {
+                timestamp += inc;
+            }
+
+            inc *= bus;
+        }
+
+        timestamp
+    }
+}
